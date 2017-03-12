14257c1a689f39d62592a9349ab288826fdf0953
himanshu13196 <himanshuyadav96@gmail.com>
2017-03-12 23:38:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req,res){
+    
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
 });
 
